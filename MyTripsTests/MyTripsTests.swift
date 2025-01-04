4c2b868831bf05c71fcdbf38e9814e99438ef373
Jeffery Ji <jefferyjijun@gmail.com>
2025-01-04 16:25:30 +0800

    Initial Commit

---
+++ MyTripsTests/MyTripsTests.swift
@@ -0,0 +1,17 @@
+//
+//  MyTripsTests.swift
+//  MyTripsTests
+//
+//  Created by Jeffery Ji on 4/1/25.
+//
+
+import Testing
+@testable import MyTrips
+
+struct MyTripsTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
